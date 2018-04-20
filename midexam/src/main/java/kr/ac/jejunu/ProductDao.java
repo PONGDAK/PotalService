ad4b7d16cda80b50ccb806578802441039320ff6
PONGDAK <31719649+PONGDAK@users.noreply.github.com>
2018-04-20 15:19:04 +0900

    midexam - no1
    상품추가기능

--- midexam/src/main/java/kr/ac/jejunu/ProductDao.java
+++ midexam/src/main/java/kr/ac/jejunu/ProductDao.java
@@ -25,4 +25,25 @@ public class ProductDao {
 
         return product;
     }
+
+    public Long insert(Product product) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju", "jeju", "jejupw");
+
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into product(title, price) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, product.getTitle());
+        preparedStatement.setInt(2, product.getPrice());
+
+        preparedStatement.executeUpdate();
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+
+        Long id = resultSet.getLong(1);
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return id;
+    }
 }
