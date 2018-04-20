1024863ff1fd119043a49c2b36b798c674e04b77
PONGDAK <31719649+PONGDAK@users.noreply.github.com>
2018-04-20 15:31:10 +0900

    midexam - no3

--- midexam/src/test/java/kr/ac/jejunu/ProductDaoTest.java
+++ midexam/src/test/java/kr/ac/jejunu/ProductDaoTest.java
@@ -9,12 +9,10 @@ import static org.junit.Assert.assertEquals;
 
 public class ProductDaoTest {
     private ProductDao productDao;
-    private ProductDao hallaProductDao;
 
     @Before
     public void setup() {
-        productDao = new JejuProductDao();
-        hallaProductDao = new HallaProductDao();
+        productDao = new ProductDao();
     }
 
     @Test
@@ -24,19 +22,6 @@ public class ProductDaoTest {
         Integer price = 15000;
 
         Product product = productDao.get(id);
-        assertEquals(id, product.getId());
-        assertEquals(title, product.getTitle());
-        assertEquals(price, product.getPrice());
-    }
-
-
-    @Test
-    public void hallaGet() throws SQLException, ClassNotFoundException {
-        Long id = 1L;
-        String title = "제주감귤";
-        Integer price = 15000;
-
-        Product product = hallaProductDao.get(id);
         assertEquals(id, product.getId());
         assertEquals(title, product.getTitle());
         assertEquals(price, product.getPrice());
