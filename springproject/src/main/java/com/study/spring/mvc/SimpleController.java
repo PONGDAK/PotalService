b5205bd76de564c0feef5f3b7e4461c1a28fd820
PONGDAK <31719649+PONGDAK@users.noreply.github.com>
2018-06-06 21:32:28 +0900

    use RequestMethodHandlerMapping(Default) //Annotaional(Deprecated)

--- springproject/src/main/java/com/study/spring/mvc/SimpleController.java
+++ springproject/src/main/java/com/study/spring/mvc/SimpleController.java
@@ -1,5 +1,6 @@
 package com.study.spring.mvc;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -8,10 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 
 //@component를 상속받은애임 컨트롤러로 지정되면 빈으로 지정되고 컨트롤러 역할을함  service , repository또한 그럼
 @org.springframework.stereotype.Controller("/helloworld")
-public class SimpleController implements Controller{
+@RequestMapping("/helloworld")
+public class SimpleController {
 
-    @Override
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    //annotaion 기반 handler adaptor
+    @RequestMapping("/hi")
+    public ModelAndView hello() {
         ModelAndView modelAndView = new ModelAndView("hello");
         modelAndView.addObject("hello", "Hello World!!!");
         return modelAndView;
