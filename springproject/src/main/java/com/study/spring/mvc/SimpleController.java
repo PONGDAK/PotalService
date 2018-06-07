5d2b8b0bf9a4886fcc27cb0c752968cc29f2e412
PONGDAK <31719649+PONGDAK@users.noreply.github.com>
2018-06-08 00:22:13 +0900

    add upload function

--- springproject/src/main/java/com/study/spring/mvc/SimpleController.java
+++ springproject/src/main/java/com/study/spring/mvc/SimpleController.java
@@ -2,11 +2,16 @@ package com.study.spring.mvc;
 
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 //@component를 상속받은애임 컨트롤러로 지정되면 빈으로 지정되고 컨트롤러 역할을함  service , repository또한 그럼
 @org.springframework.stereotype.Controller("/helloworld")
@@ -25,5 +30,24 @@ public class SimpleController {
     @ExceptionHandler(NullPointerException.class)
     public String error(NullPointerException e){
         return "error";
+    }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.GET)
+    public String upload(){
+        return "upload";
+    }
+
+    //HttpServletRequest 는 필요없는데 저장된경로 알아보려고 추가
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public ModelAndView upload(@RequestParam("file")MultipartFile file, HttpServletRequest request) throws IOException {
+        //apache commons 오픈소스 이용 파일업로드 (기본적으로 스프링이 채택하기도 했음)
+        File path = new File(request.getServletContext().getRealPath("/") + "/WEB-INF/static/" + file.getOriginalFilename());
+        FileOutputStream fileOutputStream = new FileOutputStream(path);
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+        bufferedOutputStream.write(file.getBytes());
+        bufferedOutputStream.close();
+        ModelAndView modelAndView = new ModelAndView("upload");
+        modelAndView.addObject("url", "/images/" + file.getOriginalFilename()) ;
+        return modelAndView;
     }
 }
