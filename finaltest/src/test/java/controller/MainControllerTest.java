2e41826ed0f79603d8217fd07665b72a15fdbd66
PONGDAK <31719649+PONGDAK@users.noreply.github.com>
2018-06-11 20:30:36 +0900

    make test case

---
+++ finaltest/src/test/java/controller/MainControllerTest.java
@@ -0,0 +1,34 @@
+package controller;
+
+import com.portal.service.config.WebConfig;
+import com.portal.service.config.WebInitializer;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.inject.Inject;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {WebConfig.class, WebInitializer.class})
+public class MainControllerTest {
+    @Inject
+    WebApplicationContext wac;
+    MockMvc mockMvc;
+
+    @Before
+    public void setup() throws Exception{
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+    @Test
+    public void main() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/doA"));
+    }
+}
