38955e45a750b0a2c84dd0fb18d15722ca8e07f0
PONGDAK <31719649+PONGDAK@users.noreply.github.com>
2018-06-16 16:49:17 +0900

    add encoding filter

--- finaltest/src/main/java/com/portal/service/config/WebInitializer.java
+++ finaltest/src/main/java/com/portal/service/config/WebInitializer.java
@@ -2,8 +2,10 @@ package com.portal.service.config;
 
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -11,10 +13,22 @@ import javax.servlet.ServletRegistration;
 public class WebInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
+        this.addDispatcherServlet(servletContext);
+        this.addUtf8CharacterEncodingFilter(servletContext);
+    }
+
+    private void addDispatcherServlet(ServletContext servletContext) {
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
         applicationContext.scan("com.portal.service");
         ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher", new DispatcherServlet(applicationContext));
         registration.setLoadOnStartup(1);
         registration.addMapping("/");
+    }
+
+    private void addUtf8CharacterEncodingFilter(ServletContext servletContext) {
+        FilterRegistration.Dynamic filter = servletContext.addFilter("CHARACTER_ENCODING_FILTER", CharacterEncodingFilter.class);
+        filter.setInitParameter("encoding", "UTF-8");
+        filter.setInitParameter("forceEncoding", "true");
+        filter.addMappingForUrlPatterns(null, false, "/*");
     }
 }
